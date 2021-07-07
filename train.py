27ac2e78f34ff3cd7d903b9b3b239a28069cda96
jscarlson <jcarlson238@gmail.com>
2021-07-07 19:57:29 -0400

    Revert

--- train.py
+++ train.py
@@ -4,9 +4,7 @@ from __future__ import print_function
 
 import argparse
 
-# import tensorflow as tf
-import tensorflow.compat.v1 as tf
-tf.disable_v2_behavior()
+import tensorflow as tf
 import os
 from model.preprocessing_helper import CANVAS_SIZE, EMBEDDING_DIM
 from model.unet import UNet
